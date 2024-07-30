a31e9d47f5af510af7eccb970d8bfc5ecc35b07b
Sundamo <sundamo1993@gmail.com>
2024-07-29 19:58:46 -0700

    Apis

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Using what you did in the task #0, extend your Python
+script to export data in the JSON format.
+
+Requirements:
+
+Records all tasks that are owned by this employee
+Format must be: { "USER_ID": [{"task": "TASK_TITLE", "completed":
+TASK_COMPLETED_STATUS, "username": "USERNAME"}, {"task":
+"TASK_TITLE", "completed": TASK_COMPLETED_STATUS,
+"username": "USERNAME"}, ... ]}
+File name must be: USER_ID.json
+
+"""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    emp_id = sys.argv[1]
+    url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
+    res = requests.get(url)
+    new_url = url + "/todos"
+    new_res = requests.get(new_url)
+    if new_res.status_code == 200:
+        data = res.json()
+        name = data.get('username')
+        tasks = new_res.json()
+
+        file = f"{emp_id}.json"
+        with open(file, "w") as json_file:
+            task_list = []
+            for task in tasks:
+                user_id = task.get("userId")
+                completed = task.get("completed")
+                title = task.get("title")
+                task_object = {
+                    "task": title,
+                    "completed": completed,
+                    "username": name
+                }
+                task_list.append(task_object)
+
+            json.dump({user_id: task_list}, json_file)
+    else:
+        print(f"Error: {new_res.status_code}")
